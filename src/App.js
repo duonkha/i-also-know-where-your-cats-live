d5d2ee2d9066a96f432a8affae22253472bbaf0f
duonkha <duonkha@gmail.com>
2021-08-27 22:44:10 -0700

    Display basic fullscreen leaflet map

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import {Box} from '@material-ui/core';
+
+const position = [51.505, -0.09];
+
+const MyMap = () => {
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{height: '100%', width: '100%'}}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Stuffs will come one day.
-        </p>
-      </header>
-    </div>
+    <Box height="100%">
+      <MyMap />
+    </Box>
   );
 }
 
